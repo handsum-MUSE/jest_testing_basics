d281cc8f78e2fe76fb9b55cc0b26d3965c69bc20
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-10 16:10:43 +0100

    Tested Anagram, Chunk and ReverseString.js files

--- functions.test.js
+++ functions.test.js
@@ -19,25 +19,41 @@ test('Should be falsy', () => {
 test('User should be Egbo Uchenna object', () => {
     expect(functions.createUser()).toEqual({
         firstName: 'Uchenna',
-        lastName: 'Egbo'
+        lastName: 'Egbo' 
     });
 });
 
+//  Greater Than or Less Than
 test('Should be under 1600', () => {
     const load1 = 800;
     const load2 = 700;
     expect(load1 + load2).toBeLessThan(1600);
 });
 
+// Regex
 test('There is no I in team', () => {
     expect('team').not.toMatch(/I/i);
 });
 
+// Arrays
 test('Admin should be in usernames', () => {
     const usernames = ["Karen", 'Joy', 'Admin'];
     expect(usernames).toContain('Admin');
 });
 
-test('User fetched name should be Leanne Graham', () => {
-    expect(data.name).toEqual('Leanne Graham');
-});
+// Working with Async data
+
+//Promise
+// test('User fetched name should be Leanne Graham', () => {
+//     expect.assertions(1);
+//     return functions.fetchUser().then(data => {
+//         expect(data.name).toEqual('Leanne Graham');
+//     })
+// },20000);
+
+// Async Await 
+// test('User fetched name should be Leanne Graham', async () => {
+//     expect.assertions(1);
+//     const data = await functions.fetchUser();
+//         expect(data.name).toEqual('Leanne Graham');
+// },10000);
