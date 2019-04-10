518e7d080921a8f7e1c630b41d3d13996cfacacd
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-10 12:27:00 +0100

    Ran a few unit tests

--- functions.js
+++ functions.js
@@ -1,7 +1,16 @@
 const functions = {
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
-    checkValue: x => x
+    checkValue: x => x,
+    createUser: () => {
+        const user = { firstName : 'Uchenna'}
+        user['lastName'] = 'Egbo';
+        return user;
+    },
+    fetchUser: () => axios
+    .get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -8,10 +8,32 @@ test('Add 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 });
 
-test('should be Null', () => {
+test('Should be Null', () => {
     expect(functions.isNull()).toBe(null);
 });
 
-test('should be falsy', () => {
+test('Should be falsy', () => {
     expect(functions.checkValue(null)).toBeFalsy();
 });
+
+test('User should be Egbo Uchenna object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Uchenna',
+        lastName: 'Egbo'
+    });
+});
+
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+});
+
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/i);
+});
+
+test('Admin should be in usernames', () => {
+    const usernames = ["Karen", 'Joy', 'Admin'];
+    expect(usernames).toContain('Admin');
+})
